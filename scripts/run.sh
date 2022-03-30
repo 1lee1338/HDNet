3e614d7726bfddc57618198ef3dc40f9c948fd7d
1lee1338 <lijianhao1998@163.com>
2022-03-30 09:50:27 +0800

    update

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
 # train
-#CUDA_VISIBLE_DEVICES=2 python train.py --model hdnet --backbone resnet50 --dataset inria --name wei-20-lr001  --lr 0.01 --aux-weight 20  --epochs 200 --batch-size 16  --save-epoch 5
-#CUDA_VISIBLE_DEVICES=2 python train.py --model hdnet --backbone resnet50 --dataset whu_aerial --name wei-20-lr001  --lr 0.01 --aux-weight 20  --epochs 200 --batch-size 16  --save-epoch 5
-#CUDA_VISIBLE_DEVICES=3 python train.py --model hdnet --backbone resnet50 --dataset whu_satellite --name wei-20-lr001  --lr 0.01 --aux-weight 20  --epochs 200 --batch-size 16  --save-epoch 5
+#CUDA_VISIBLE_DEVICES=2 python train.py --model hdnet --backbone resnet50 --dataset inria --name wei-20-lr001  --lr 0.01 --aux-weight 20  --epochs 200 --batch-size 8  --save-epoch 5
+#CUDA_VISIBLE_DEVICES=2 python train.py --model hdnet --backbone resnet50 --dataset whu_aerial --name wei-20-lr001  --lr 0.01 --aux-weight 20  --epochs 200 --batch-size 8  --save-epoch 5
+#CUDA_VISIBLE_DEVICES=3 python train.py --model hdnet --backbone resnet50 --dataset whu_satellite --name wei-20-lr001  --lr 0.01 --aux-weight 20  --epochs 200 --batch-size 8  --save-epoch 5
 
 
 
